4df770b003a99e0afb66fa1ae1bf6f39b5ec932e
Lordemax <lordemax2012@gmail.com>
2016-06-16 01:50:39 +0100

    Add files via upload

---
+++ reset.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Saya ga tau bash scripting
+# Harap maklum hehehehe...
+echo "-------------------------------"
+echo "USERNAME          EXP DATE     "
+echo "-------------------------------"
+while read mumetndase
+do
+        AKUN="$(echo $mumetndase | cut -d: -f1)"
+        ID="$(echo $mumetndase | grep -v nobody | cut -d: -f3)"
+        exp="$(chage -l $AKUN | grep "Account expires" | awk -F": " '{print $2}')"
+        if [[ $ID -ge 1000 ]]; then
+        printf "%-17s %2s\n" "$AKUN" "$exp"
+        fi
+done < /etc/passwd
+JUMLAH="$(awk -F: '$3 >= 1000 && $1 != "nobody" {print $1}' /etc/passwd | wc -l)"
+echo "-------------------------------"
+echo "Jumlah akun: $JUMLAH user"
+echo "-------------------------------"
+echo -e "\e[1;33;44m[ ari-f.com ]\e[0m"
+
